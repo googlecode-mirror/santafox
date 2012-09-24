d052414481dd03159cd76916e42670d0ed465f36
oslix <oslix@yandex.ru>
2012-09-24 12:45:30 +0000

    Подправил

--- sinstall/sql/santa_stat_host.sql
+++ sinstall/sql/santa_stat_host.sql
@@ -21,4 +21,4 @@ CREATE TABLE `%PREFIX%_stat_host` (
   KEY `tstc` (`tstc`),
   KEY `IDSess` (`IDSess`),
   KEY `f_people` (`f_people`)
-) ENGINE=MyISAM AUTO_INCREMENT=854 DEFAULT CHARSET=utf8 COMMENT='Статистика - хосты';
+) ENGINE=MyISAM AUTO_INCREMENT=854 DEFAULT CHARSET=utf8 COMMENT='Статистика хостов';
